5a6be7920d2da3315bd29a91072a06b62edbccc7
mrfarhadi <mor.farhadi@gmail.com>
2016-04-02 21:44:24 -0400

    base

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer();
+var app = express();
+var isProduction = process.env.NODE_ENV === 'production';
+var port = isProduction ? process.env.PORT : 8080;
+var publicPath = path.join(__dirname, '../dist');
+
+if (isProduction) {
+  app.use(express.static(publicPath));
+  app.get('/', function (req, res) {
+    res.sendFile('index.html', {
+      root: static_path
+    });
+  });
+}
+
+app.get('/v1/test', function (req, res) {
+  res.send('back-end works');
+});
+
+var server = http.createServer(app).listen(port, function() {
+  console.log('Server running on port ' + port);
+});
+
---
+++ src/app/home/home.ts
@@ -0,0 +1,15 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'home',  // <home></home>
+  template: require('./home.html')
+})
+export class Home {
+  constructor() {
+  }
+
+  ngOnInit() {
+    console.log('hello `Home` component');
+  }
+
+}
