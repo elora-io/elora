ae73b510a97babe519326e071c4376f96f401805
Ahmed Abouzeid <ahmed.adham88@gmail.com>
2016-04-07 16:09:14 -0400

    Removing useless code in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,2 @@
 // Look in ./config folder for webpack.dev.js
-var webpack = require('./config/webpack.prod.js');
-
-var config = {
-	module: {
-	    loaders: [
-	    	{
-			  test: /\.(jpe?g|png|jpg|gif|svg)$/i,
-			  loader:'file'
-			}
-	    ]
-  	}
-}
-
-module.exports = config;
+module.exports = require('./config/webpack.prod.js');
